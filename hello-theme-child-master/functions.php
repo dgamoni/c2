5563f61ee90f0f647cd2050e2cb84ec77f3fc928
dgamoni <dgamoni@gmail.com>
2024-03-25 03:05:51 +0100

    first commit

---
+++ hello-theme-child-master/functions.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ * @package HelloElementorChild
+ */
+
+/**
+ * Load child theme css and optional scripts
+ *
+ * @return void
+ */
+function hello_elementor_child_enqueue_scripts() {
+	wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		'1.0.0'
+	);
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts' );
+
+
+/**
+ * Add Font Group
+ */
+add_filter( 'elementor/fonts/groups', function( $font_groups ) {
+	$font_groups['theme_fonts'] = __( 'Theme Fonts' );
+	return $font_groups;
+} );
+
+/**
+ * Add Group Fonts
+ */
+add_filter( 'elementor/fonts/additional_fonts', function( $additional_fonts ) {
+	// Key/value
+	//Font name/font group
+	$additional_fonts['DM Sans'] = 'theme_fonts';
+	return $additional_fonts;
+} );
